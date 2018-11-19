e251ffd7315a8f797aa83985b47e3824479bd7ec
kylefred <kfg14@my.fsu.edu>
2018-11-18 20:56:28 -0500

    Delete hw9_3.py

--- hw9_3.py
+++
@@ -1,9 +0,0 @@
-import pandas
-import numpy
-
-numpy.random.seed(0)
-
-data = numpy.random.rand(20, 5)
-df = pandas.DataFrame(data = data, columns = ['Col1','Col2','Col3','Col4','Col5'])
-
-print(df.mean())
